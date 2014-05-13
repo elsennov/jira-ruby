d0f1c6b826e5fec83e8a74ea47d257374908f770
Elsen <elsen@icehousecorp.com>
2014-05-13 17:39:19 +0700

    Add max result for issues

--- lib/jira/resource/project.rb
+++ lib/jira/resource/project.rb
@@ -18,7 +18,7 @@ module JIRA
       # Returns all the issues for this project
       def issues(options={})
         search_url = client.options[:rest_base_path] + '/search'
-        query_params = {:jql => "project=\"#{key}\""}
+        query_params = {:jql => "project=\"#{key}\"", :startAt => 0, :maxResults => 1000}
         query_params.update Base.query_params_for_search(options)
         response = client.get(url_with_query_params(search_url, query_params))
         json = self.class.parse_json(response.body)
